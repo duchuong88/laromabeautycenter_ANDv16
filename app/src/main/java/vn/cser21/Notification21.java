b87ef5f8ce33119baba7f6a36549381caa05b457
NguyenTuanEZS <nguyentuan@ezs.vn>
2024-07-19 15:13:10 +0700

    first commit

---
+++ app/src/main/java/vn/cser21/Notification21.java
@@ -0,0 +1,8 @@
+package laromabeauty.ezs;
+
+
+public  class Notification21 {
+    public String title;
+    public String body;
+    public String sound;
+}
---
+++ app/src/main/java/vn/cser21/incoming/EndCallEvent.java
@@ -0,0 +1,3 @@
+package laromabeauty.ezs.incoming;
+
+public class EndCallEvent { }
